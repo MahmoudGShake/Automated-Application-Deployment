f7467558f1878d4a674ceb3e6f05750757c1efb4
nourGAD <nour0gad@gmail.com>
2025-12-07 14:27:15 +0300

    After SSuobject

--- website/forms.py
+++ website/forms.py
@@ -23,7 +23,7 @@ class ContactForm(forms.Form):
     }))
     subject = forms.CharField(max_length=200, widget=forms.TextInput(attrs={
         "class": "form-control",
-        "placeholder": "Subject"
+        "placeholder": "SSubject"
     }))
     message = forms.CharField(widget=forms.Textarea(attrs={
         "class": "form-control",
